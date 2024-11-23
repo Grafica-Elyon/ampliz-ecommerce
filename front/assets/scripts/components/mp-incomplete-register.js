8af888fca70fc2335833cf1aa194442dc57eff77
Willian Rösner <willianrosner@gmail.com>
2024-11-23 12:09:31 -0300

    ajuste js page

--- front/assets/scripts/components/mp-incomplete-register.js
+++ front/assets/scripts/components/mp-incomplete-register.js
@@ -2,7 +2,13 @@ import Components from '../Components';
 import Form from '../Form'
 
 export default function (el) {
-	var rules = [
+	// Identificando qual formulário está sendo manipulado
+	const isRegisterStep1 = $(el).hasClass('register-step1');
+	const isRegisterStep2 = $(el).hasClass('register-step2');
+	const isLoginForm = $(el).hasClass('login-form');
+
+	// Regras para a primeira tela do registro (email e telefone)
+	const registerStep1Rules = [
 		{
 			'name': 'email',
 			'rules': {
@@ -16,53 +22,104 @@ export default function (el) {
 			}
 		},
 		{
-			'name': 'confirm-email',
+			'name': 'telefone',
 			'rules': {
 				stop: true,
-				title: 'E-mail',
-				equal: {
-					value: '',
-					field: 'Confirmaçao de e-mail',
-					select: '[name="email"]',
-					message: '{field} invalida!'
+				required: {
+					message: 'Telefone é obrigatório!',
 				}
 			}
-		},
+		}
+	];
+
+	// Regras para o segundo passo do registro
+	const registerStep2Rules = [
 		{
 			'name': 'nome-completo',
 			'rules': {
 				stop: true,
 				required: {
-					message: 'O nome é obrigatorio!',
+					message: 'O nome é obrigatório!',
 				}
 			}
 		},
+		// Adicione outras regras para os campos do segundo passo aqui
 	];
 
-	var getInputs = () => {
+	const loginRules = [
+		{
+			'name': 'email',
+			'rules': {
+				stop: false,
+				required: {
+					message: 'E-mail é obrigatório!',
+				},
+				email: {
+					message: 'E-mail inválido',
+				}
+			}
+		},
+		{
+			'name': 'senha',
+			'rules': {
+				stop: true,
+				required: {
+					message: 'A senha é obrigatória!',
+				}
+			}
+		}
+	];
 
-		var values = {};
-		$(' input, select', el).each((event, element) => {
+	const getInputs = () => {
+		const values = {};
+		$('input, select', el).each((event, element) => {
 			values[$(element).attr('name')] = $(element).val();
 		});
+
+		if (isRegisterStep1) {
+			values.form_type = 'register_step1';
+		} else if (isRegisterStep2) {
+			values.form_type = 'register_step2';
+		} else if (isLoginForm) {
+			values.form_type = 'login';
+		}
+
 		return values;
 	};
 
-	var registerEvents = () => {
+	const registerEvents = () => {
+		let rules;
+		if (isRegisterStep1) {
+			rules = registerStep1Rules;
+		} else if (isRegisterStep2) {
+			rules = registerStep2Rules;
+		} else if (isLoginForm) {
+			rules = loginRules;
+		}
+
 		new Form(el, rules, (form) => {
 			Components.loading(el);
 
-			var inputs = getInputs();
+			const inputs = getInputs();
+			let action = isLoginForm ? 'mp_login' : 'mp_incomplete_register';
+
 			$.ajax({
 				method: "POST",
 				url: wp.ajax_url,
 				data: {
-					action: 'mp_incomplete_register',
+					action: action,
 					'data': inputs,
 					'params': inputs.params,
 				}
 			}).done((response) => {
-				if (typeof response.redirect != 'undefined') {
+				if (typeof response.error !== 'undefined') {
+					const errorContainer = $(el).find('.error-message');
+					errorContainer.html(response.error).show();
+					Components.loading(el, 'stop');
+					return;
+				}
+
+				if (typeof response.redirect !== 'undefined') {
 					window.location = response.redirect;
 					return;
 				}
@@ -71,10 +128,13 @@ export default function (el) {
 				registerEvents();
 
 				Components.loading(el, 'stop');
+			}).fail((jqXHR, textStatus, errorThrown) => {
+				const errorContainer = $(el).find('.error-message');
+				errorContainer.html('Ocorreu um erro. Por favor, tente novamente.').show();
+				Components.loading(el, 'stop');
 			});
 		});
 	}
 
 	registerEvents();
-
 }
